b198d68062d9d846f493ada3f4145057a933f55a
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2024-03-06 21:09:41 +0530

    Add files via upload

--- Question05.java
+++ Question05.java
@@ -1,6 +1,4 @@
-package q10759;
-public class PrintHello {
-	public static void main(String[] args) {
-		System.out.println("Hello, I am learning Java!");
-	}
-}
+Select all the correct answers
+Answer 
+
+In Binary System, decimal 3 is represented as 0b11
