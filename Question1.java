bc1339b120a4722ee3fb8c9bc52d60efe7509032
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2024-03-15 23:58:14 +0530

    Add files via upload

--- Question1.java
+++ Question1.java
@@ -1,11 +1,27 @@
-Click on the below Live Demo button to learn the steps involved in writing and executing a Java program.
+In any programming language, a variable is a named storage location. That name is also called as identifier. For example:
+int age = 25;
+String name = "Bhargo";
+Java variables names must start with a character and can include digits. We will learn more about Java Naming and Coding conventions later.
 
+Java being a strongly typed language, every variable has a type associated.
 
+In Java, there are two types of variables
+ Primitive Type ( henceforth we will refer to them simply as variables)
+ Reference Type ( henceforth we will refer to them as references)
+The primitive types are boolean and the remaining numeric types (byte, short, int, long, float, double and char)
 
-After learning from the Live Demo, select all correct statements given below:
+The reference types can be of any class or of type array.
+
+For example, in the earlier code snippet, we will refer to age as a variable since it is of a primitive type (int).
+Similarly we refer to name as a reference, since it is of type String class.
+
+Select all the correct statements that apply to the below code.
+char gender = 'M';
+Date today = new Date();
+byte[]  buffer = new byte[1024];
+String[] namesArr = {"Apollo", "Vishnu", "Minerva", "Saraswati"};
 
 Answer 
 
-The Java compiler uses the Java source file and generates file with .class extension.
-The file with .class extension is called as Java class file.
-The Java class file contains the Java bytecode, which is executed by the Java Virtual Machine (JVM).
+buffer is a reference
+namesArr is a reference
--- Question2.java
+++ Question2.java
@@ -1,30 +1,24 @@
-JRE stands for Java Runtime Environment.
+In the below code
+double x = 2.3d;
+String name = "Bhargo";
+byte[] buffer = new byte[1024];
+x is called a variable
+name is called a reference
+buffer is called a reference
+A variable can be compared to a cup, which holds the value of the primitive date type.
+A reference can be compared to a cup, which holds the address of the object it points to.
 
-Java Runtime Environment which is commonly referred JRE, is the essential piece of software which is required to run any Java program on an operating system.
-
-Note that JDK (which stands for Java Development Kit) also contains in it a JRE along with other tools like compilers which are required during the development phase.
-
-While, JRE contains only those tools and libraries which are essential for running a Java program.
-
-Click here to gain a better understanding of JDK and JRE.
-
-JDK and JRE are available as separate downloads. Meaning, if one downloads and installs a JDK, JRE is automatically installed as it is bundled along with it.
-
-However, when a JRE is separately downloaded an installed on a OS, only the JRE's content is installed, meaning the compilers, etc, which are part of JDK will not be present.
-
-When one wants to write a Java program, since one also needs to compile it before running it, JDK is needed.
-
-However, when one gets a compiled Java program in an executable form (from somewhere else on to his OS), a JRE is sufficient to run it.
-
-JRE is a bundle of programs that mainly includes
-JVM (Java VM or Java Virtual Machine)
-Browser plugin for running Java Applets
-Java class libraries (these class libraries contain standard Java classes used during execution) and configuration tools
-Select all correct statements given below:
-
-[Hint: You can click on the words in blue to know more about them.]
+When not initialized, the default value of a variable is 0 for numeric primitive types and false for boolean types.
+When not initialized, the default value of a reference is null. If a reference has a null value, it means that the reference does not refers (points) to any object.
+Select all the correct statements that apply to the below code.
+String name = null;
+int age = null;
+byte[] buffer = null;
+boolean status = null;
 
 
 Answer 
 
-Java class libraries contain the class files of standard Java classes required for running Java Applications or Applets.
+
+Statement String name = null; is correct
+Statement byte[] buffer = null; is correct
